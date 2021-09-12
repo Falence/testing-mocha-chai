13065667dc4f492826b0c612dee3cd6dee627891
Falence Lemungoh <falencelemungoh@gmail.com>
2021-09-13 00:42:02 +0100

    testing code with an active database

--- test/auth-controller.js
+++ test/auth-controller.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose')
 const { expect } = require('chai')
 const sinon = require('sinon')
 
@@ -19,10 +20,49 @@ describe('Auth Controller - Login', function() {
     authController.login(req, {}, () => {})
       .then(result => {
         expect(result).to.be.an('error')
-        expect(result).to.have.property('statusCode', 400)
+        expect(result).to.have.property('statusCode', 500)
         done()
       })
 
     User.findOne.restore()
+  })
+
+  it('should send a response with a valid user status for an existing user', function(done) {
+    mongoose
+      .connect(
+        'mongodb://localhost:27017/mochas-test-db'
+      )
+      .then(result => {
+        const user = new User({
+          email: 'tester@tester.com',
+          password: 'tester',
+          name: 'test',
+          post: [],
+          _id: '5c0f66b979af55031b34728a'
+        })
+        return user.save()
+      })
+      .then(() => {
+        const req = { userId: '5c0f66b979af55031b34728a' }
+        const res = {
+          statusCode: 500,
+          userStatus: null,
+          status: function(code) {
+            this.statusCode = code
+            return this
+          },
+          json: function(data) {
+            this.userStatus = data.status
+          }
+        }
+
+        authController.getUserStatus(req, res, () => {})
+          .then(() => {
+            expect(res.statusCode).to.be.equal(200)
+            expect(res.userStatus).to.be.equal('I am new!')
+            done()
+          })
+      })
+      .catch(err => console.log(err));
   })
 })
